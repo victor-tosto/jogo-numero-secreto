28117e446ec03de76065fb3597c55d86502f65d9
Victor Morales <72416141+victor-tosto@users.noreply.github.com>
2023-07-10 18:43:26 -0300

    game over adicionado

--- app/validacao.js
+++ app/validacao.js
@@ -1,31 +1,42 @@
 function verificaSeOChutePossuiUmValorValido(chute) {
 
-    const numero = +chute;
-
-    if (seOChuteForInvalido(numero)) {
-        elementoChute.innerHTML += '<div>Valor inválido</div>';
-        return;
-    }
-
-    if (numeroForMaiorOuMenorQueOValorPermitido(numero)) {
-        elementoChute.innerHTML += `<div>Valor inválido: o número precisa estar entre ${menorValor} e ${maiorValor}.</div>`;
-
-        return;
-    }
-
-    if (numero === numeroSecreto) {
+    if (chute == 'game over') {
         document.body.innerHTML = `
-            <h2>Você acertou!</h2>
-            <h3>O número secreto era ${numeroSecreto}</h3>
+            <h2>GAME OVER</h2>
+            <h3>O jogo foi encerrado</h3>
 
             <button id="jogar-novamente" class="btn-jogar">Jogar novamente</button>
         `;
     }
-    else if (numero > numeroSecreto) {
-        elementoChute.innerHTML += `<div>O número secreto é menor <i class="fa-solid fa-arrow-down-long"></i></div>`
-    }
     else {
-        elementoChute.innerHTML += `<div>O número secreto é maior <i class="fa-solid fa-arrow-up-long"></i></div>`
+
+        const numero = +chute;
+
+        if (seOChuteForInvalido(numero)) {
+            elementoChute.innerHTML += '<div>Valor inválido</div>';
+            return;
+        }
+
+        if (numeroForMaiorOuMenorQueOValorPermitido(numero)) {
+            elementoChute.innerHTML += `<div>Valor inválido: o número precisa estar entre ${menorValor} e ${maiorValor}.</div>`;
+
+            return;
+        }
+
+        if (numero === numeroSecreto) {
+            document.body.innerHTML = `
+                <h2>Você acertou!</h2>
+                <h3>O número secreto era ${numeroSecreto}</h3>
+
+                <button id="jogar-novamente" class="btn-jogar">Jogar novamente</button>
+            `;
+        }
+        else if (numero > numeroSecreto) {
+            elementoChute.innerHTML += `<div>O número secreto é menor <i class="fa-solid fa-arrow-down-long"></i></div>`
+        }
+        else {
+            elementoChute.innerHTML += `<div>O número secreto é maior <i class="fa-solid fa-arrow-up-long"></i></div>`
+        }
     }
 
 }
